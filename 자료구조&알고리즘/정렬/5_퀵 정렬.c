f6c7c1c2e5327aab0a313d1ce15bfe1c7ea6aa91
gitul0515 <gitul0515@gmail.com>
2021-09-17 19:59:20 +0900

    퀵 정렬

--- 자료구조&알고리즘/정렬/5_퀵 정렬.c
+++ 자료구조&알고리즘/정렬/5_퀵 정렬.c
@@ -2,9 +2,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#define MAX_SIZE 8
+#define MAX_SIZE 10
 #define SWAP(X,Y,T) ((T)=(X),(X)=(Y),(Y)=(T))
 
+void print_list(int* list, int pivot);
 void quick_sort(int* list, int left, int right);
 int partition(int* list, int left, int right);
 
@@ -13,22 +14,27 @@ int main()
 	int i;
 	int num = MAX_SIZE;
 	int list[MAX_SIZE];
+
+	// 난수를 생성하여 배열에 저장한다.
+	printf("초기값: ");
 	srand(time(NULL));
-	for (i = 0; i < num; i++) // 8개의 난수 생성
+	for (i = 0; i < num; i++) {
 		list[i] = (rand() % 100) + 1; // 난수 범위: 1~100
-	
-	quick_sort(list, 0, num - 1); //퀵 정렬 호출
-								  //첫 인덱스와 마지막 인덱스 전달
-	for (i = 0; i < num; i++)
 		printf("%d ", list[i]);
-	printf("\n");
-	return 0; 
+	}
+	printf("\n\n");
+
+	// 퀵 정렬 호출
+	// 배열의 주소, 시작 인덱스, 마지막 인덱스를 전달한다.
+	quick_sort(list, 0, num - 1); 
+
+	return 0;
 }
 
 void quick_sort(int* list, int left, int right)
 {
 	int q;
-	if (left < right) //리스트의 요소가 2개 이상일 때
+	if (left < right)   //리스트의 요소가 2개 이상일 때
 	{
 		q = partition(list, left, right);
 		quick_sort(list, left, q - 1);
@@ -38,21 +44,33 @@ void quick_sort(int* list, int left, int right)
 
 int partition(int* list, int left, int right)
 {
-	int low, high, pivot, temp;
-	low = left;       //do-while문을 고려하여 값을 할당
-	high = right + 1; //low는 left+1부터, high는 right부터 시작
-	pivot = left;     //pivot에는 첫번째 요소를 할당
-	do
-	{
-		do
-			low++;
-		while (list[low] < list[pivot]); // 리스트의 범위를 초과하면 거짓으로 판정
-		do                               // 값이 같을 경우에도 거짓으로 판정(안정성)
-			high--;
-		while (list[high] > list[pivot]);
-		if (low < high) SWAP(list[low], list[high], temp);
-	} while (low < high);
-	
-	SWAP(list[pivot], list[high], temp);
-	return high;	  //pivot값이 저장된 인덱스를 반환
+	int pivot = list[right];
+	int i = left - 1;
+	int temp;
+
+	for (int j = left; j < right; j++)
+		if (list[j] <= pivot) {
+			i++;
+			SWAP(list[i], list[j], temp);
+		}
+	SWAP(list[i + 1], list[right], temp);
+
+	// 현재까지의 정렬 결과를 출력한다.
+	print_list(list, pivot);
+
+	return i + 1; // 피벗의 위치를 반환한다.
+}
+
+// 정렬의 결과를 출력하는 함수
+void print_list(int* list, int pivot) {
+	int i;
+	static int level = 0; // level은 partition 함수가 실행된 횟수
+
+	printf("level %d\n", ++level);
+	printf("========================================\n");
+	printf("pivot: %d\n", pivot);
+	printf("result: ");
+	for (i = 0; i < MAX_SIZE; i++)
+		printf("%d ", list[i]);
+	printf("\n\n");
 }
