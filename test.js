db0c612c6aee6a5e445e81a111343c84f536ddcc
gitul0515 <gitul0515@gmail.com>
2021-09-23 21:51:18 +0900

    퀵 정렬

--- test.js
+++ test.js
@@ -1,8 +1,21 @@
-const startAlert = function () {
-  const playAlert = setInterval(function () {
-    alert('Hello!');
-  }, 5000);
+const obj = {
+  name: 'Kwon',
+  age: 28,
+  address: 'Ansan',
+  sayHello() {
+    return `Hi, my name is ${this.name}.`;
+  }
 };
-const stopAlert = function () {
-  clearInterval(playAlert);
-};
+
+console.log(Object.entries(obj));
+console.log();
+console.log(Object.keys(obj));
+console.log();
+console.log(Object.values(obj));
+console.log();
+
+for (const key in obj) {
+  if (Object.hasOwnProperty.call(obj, key)) {
+    console.log(key, obj[key]);
+  }
+}
