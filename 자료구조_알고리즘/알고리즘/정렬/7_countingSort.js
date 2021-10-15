0a2048aa8dfc8b1320bf5c9b3225eac4c3406eb6
gitul0515 <gitul0515@gmail.com>
2021-10-15 20:12:41 +0900

    계수정렬 리팩토링

--- 자료구조_알고리즘/알고리즘/정렬/7_countingSort.js
+++ 자료구조_알고리즘/알고리즘/정렬/7_countingSort.js
@@ -1,28 +1,28 @@
 // 계수 정렬 알고리즘 (시간 복잡도: O(n + k))
-// 배열 생성 및 초기화
-const preArray = []; // 초기 배열
+// 초기 배열
+const preArray = [];
 for (let i = 0; i < 30; i++) { // 30개의 난수 생성
   preArray[i] = Math.floor(Math.random()*100 + 1); // 난수 범위: 1 ~ 100;
 }
-const numMax = 100; // 난수의 최대값
+const numMax = 100; // 난수 범위의 최대값
 console.log('초기 배열: ', preArray);
 
-// count 배열 생성 및 초기화
-const count = [];
-for (let i = 0; i < numMax + 1; i++) { // 인덱스 0은 사용하지 않는다.
-  count[i] = 0;
+// count 배열
+const count = [ null ]; // 인덱스 0은 사용하지 않는다.
+for (let i = 1; i <= numMax; i++) {
+  count[i] = 0;  // 인덱스 1부터 0으로 초기화
 }
-// count 배열에 값을 저장
 for (let i = 0; i < 30; i++) {
-  count[preArray[i]]++; // ex) count[5]++
+  count[preArray[i]]++;  // 예: preArray[3]의 값이 5일 때, count[5]의 값을 1 증가시킨다.
 }
 console.log('count 배열: ', count);
 
-const postArray = []; // 정렬 후 배열 생성
+// 정렬 후 배열
+const postArray = [];
 let k = 0;
-for (let i = 1; i < numMax + 1; i++) {
+for (let i = 1; i <= numMax; i++) {
   for (let j = 0; j < count[i]; j++) {
-    postArray[k++] = i;
+    postArray[k++] = i; // 예: count[5]의 값이 2라면, postArray 배열에 5를 2번 삽입한다. 
   }
 }
 console.log('정렬 후 배열: ', postArray);
