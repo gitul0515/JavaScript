a0e5b4be1444b561ea1c073b49a54931ca7e6f69
gitul0515 <gitul0515@gmail.com>
2021-10-27 15:01:38 +0900

    선택 알고리즘 수정

--- 자료구조_알고리즘/알고리즘/탐색/test2.js
+++ 자료구조_알고리즘/알고리즘/탐색/test2.js
@@ -1,48 +1,64 @@
-// Median of Medians 알고리즘 (정렬 메서드 사용)
-// 배열 생성 및 초기화
-const array = [];
-for (let i = 0; i < 32; i++) {
-  array[i] = Math.floor(Math.random() * 100); // 난수 범위: 0~99
-}
-console.log(array);
+const data = [21, 10, 34, 41, 30, 12, 19, 7, 43, 28, 56, 50, 91, 83, 81, 75]; // 샘플 데이터
+const target = 45; // 기준점
 
-// 배열의 두 요소 값을 교환하는 함수
+ // 기준점 45와의 차이를 계산하여 distance 배열에 저장
+const distance = [];
+for (let i = 0; i < data.length; i++) {
+  distance[i] = Math.abs(data[i] - 45); // 절대값을 저장한다
+}
+
+const answer = linearSelect(distance, 0, distance.length - 1, 10); // 30
+
+// distance 배열을 다시 초기화한다
+for (let i = 0; i < data.length; i++) {
+  distance[i] = Math.abs(data[i] - 45); // 절대값을 저장한다
+}
+
+let i;
+for (i = 0; i < distance.length; i++) {
+  if (distance[i] === answer) 
+    break;
+}
+
+console.log(answer);
+console.log(i);
+console.log('결과:', data[i]);
+
+// 기본적인 선택 알고리즘
+// 배열 array[l...r]에서 i번째로 작은 수를 찾는다
+function linearSelect(array, l, r, i) {
+  // 배열의 원소가 1개뿐이라면, 그 원소를 반환한다.
+  if (l === r) return array[l];
+
+  // 마지막 원소를 피벗으로 하여, 파티션 정렬을 수행한다.
+  const q = partition(array, l, r);
+
+  // 피벗은 배열에서 k번째로 작은 원소다.
+  const k = q - l + 1;
+
+  // 피벗보다 작은 원소를 찾으면 왼쪽으로 범위를 좁힌다.
+  if (i < k) return linearSelect(array, l, q - 1, i);
+  else if (i === k) return array[q]; // i와 k가 일치한다면, 피벗이 바로 찾는 원소다.
+  else return linearSelect(array, q + 1, r, i - k); // 오른쪽으로 범위를 좁힌다.
+}
+
+// 파티션 함수
+// 피벗보다 작은 요소는 왼쪽, 큰 요소는 오른쪽으로 정렬한다
+function partition(array, l, r) {
+  const pivot = array[r]; // 마지막 원소를 피벗으로 할당한다
+  let i = l - 1;
+
+  for (let j = l; j < r; j++) {
+    if (array[j] <= pivot) {
+      swap(array, ++i, j);
+    }
+  }
+  swap(array, i + 1, r);
+  return i + 1;
+}
+
 function swap(array, x, y) {
   const temp = array[x];
   array[x] = array[y];
   array[y] = temp;
-}
-
-// 배열 array[l...r]의 중앙값을 찾아 피벗으로 반환한다.
-function mideanOfMedians(array, l, r) {
-  const num = r - l + 1; // 배열의 원소 개수
-
-  // 배열의 원소가 5개 이하면
-  // 배열을 정렬한 뒤, 중앙값을 피벗으로 반환한다.
-  if (num <= 5) {
-    array.sort((a, b) => a - b); // 오름차순 정렬
-    return array[Math.floor(r / 2)];
-  }
-
-  // 배열을 5개씩의 원소를 가진 m개의 하위 배열로 나눈다.
-  const m = Math.ceil(num / 5);
-
-  const median = []; // 각 하위 배열의 중앙값을 저장할 배열 선언
-
-  for (let i = 0; i < m; i++) {
-    // 마지막 하위 배열이 아닌 경우 (원소 5개)
-    if (5 * (i + 1) < num) {
-      let subArray = array.slice(5*i, 5*(i + 1)); // 하위 배열을 임시적으로 만든 후 정렬한다.
-      subArray.sort((a, b) => a - b);
-      median[i] = subArray[2]; // 하위배열의 중앙값을 median에 저장한다.
-    } else { // 마지막 하위 배열인 경우 (원소 5개 이하)
-      let subArray = array.slice(5*i); // 하위 배열을 임시적으로 만든 후 정렬한다.
-      subArray.sort((a, b) => a - b);
-      const Mid = Math.floor((subArray.length - 1) / 2);  // 하위배열의 중앙값을 구해 median에 저장한다.
-      median[i] = subArray[Mid];
-    }
-  }
-
-  // 중앙값의 중앙값을 재귀적으로 구한다.
-  return mideanOfMedians(median, 0, m - 1);
 }
