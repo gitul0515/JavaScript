5bd7085195bbd11df774b6d75968f1c33616a94e
gitul0515 <gitul0515@gmail.com>
2021-11-27 16:11:41 +0900

    행렬경로 bottom-up

--- 자료구조_알고리즘/알고리즘/동적프로그래밍/2_행렬경로.js
+++ 자료구조_알고리즘/알고리즘/동적프로그래밍/2_행렬경로.js
@@ -8,21 +8,49 @@ const matrix = [
   [7, 17, 3, 3],
   [8, 10, 14, 9]
 ];
-const memo = [];
-for (let i = 0; i < 4; i++) {
-  memo[i] = new Array(4).fill(-1);
-}
 
+// matrix의 (i, j)에 이르는 경로의 최소합
 function matrixPath(i, j) {
-  if (memo[i][j] > -1) return memo[i][j];
+  // 1) memoizaion
+  const memo = [];
+  for (let i = 0; i < 4; i++) {
+    memo[i] = new Array(4).fill(-1);
+  }
 
-  if (i === 0 && j === 0) memo[i][j] = matrix[i][j];
-  else if (i === 0) memo[i][j] = matrixPath(i, j - 1) + matrix[i][j];
-  else if (j === 0) memo[i][j] = matrixPath(i - 1, j) + matrix[i][j];
-  else memo[i][j] = Math.min(matrixPath(i, j - 1), matrixPath(i - 1, j)) + matrix[i][j];
-  return memo[i][j];
+  return (function recursion(i, j) {
+    if (memo[i][j] > -1) return memo[i][j];
+
+    if (i === 0 && j === 0) memo[i][j] = matrix[i][j];
+    else if (i === 0) memo[i][j] = recursion(i, j - 1) + matrix[i][j];
+    else if (j === 0) memo[i][j] = recursion(i - 1, j) + matrix[i][j];
+    else memo[i][j] = Math.min(recursion(i, j - 1), recursion(i - 1, j)) + matrix[i][j];
+    return memo[i][j];
+  }(i, j));
 }
 console.log(matrixPath(0, 0)); // 6
 console.log(matrixPath(0, 2)); // 25
 console.log(matrixPath(2, 0)); // 18
 console.log(matrixPath(1, 2)); // 25
+
+// matrix의 (i, j)에 이르는 경로의 최소합
+function matrixPath2(i, j) {
+  // 2) bottom-up
+  const M = [];
+  for (let i = 0; i < 4; i++) {
+    M[i] = new Array(4);
+  }
+
+  for (let i = 0; i < M.length; i++) {
+    for (let j = 0; j < M.length; j++) {
+      if (i === 0 && j === 0) M[i][j] = matrix[i][j];
+      else if (i === 0) M[i][j] = M[i][j - 1] + matrix[i][j];
+      else if (j === 0) M[i][j] = M[i - 1][j] + matrix[i][j];
+      else M[i][j] = Math.min(M[i][j - 1], M[i - 1][j]) + matrix[i][j];
+    }
+  }
+  return M[i][j];
+}
+console.log(matrixPath2(0, 0)); // 6
+console.log(matrixPath2(0, 2)); // 25
+console.log(matrixPath2(2, 0)); // 18
+console.log(matrixPath2(1, 2)); // 25
