203a1d311b816cb83faeced07088555b9430259f
gitul0515 <gitul0515@gmail.com>
2021-12-29 23:57:07 +0900

    코딩테스트 문제 풀이

--- test.js
+++ test.js
@@ -1,11 +1,7 @@
-// 얕은 복사
-const array1 = [2, 1];
-const array2 = [...array1];
-const array3 = array1;
+const box = document.querySelector('.box');
+const rainbow = document.querySelector('.rainbow');
 
-console.log(array1); // [2, 1]
-console.log(array2); // [2, 1]
-console.log(array3); // [2, 1]
+box.addEventListener('click', () => {
+  rainbow.classList.toggle('show');
+})
 
-console.log(array1 === array2); // false: array1과 array2는 다른 객체를 가리킨다
-console.log(array1 === array3); // true: array1과 array3는 같은 객체를 가리킨다
