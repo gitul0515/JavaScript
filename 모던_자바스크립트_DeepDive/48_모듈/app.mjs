1184c34d796bfdb931b8491417795799ec3fff3c
gitul0515 <gitul0515@gmail.com>
2022-01-03 14:06:01 +0900

    모듈

---
+++ 모던_자바스크립트_DeepDive/48_모듈/app.mjs
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/extensions
+import test from './lib.mjs';
+
+console.log('test', test(2));
---
+++ 모던_자바스크립트_DeepDive/48_모듈/lib.mjs
@@ -0,0 +1,17 @@
+// 변수
+const pi = Math.PI;
+
+// 함수
+function square(x) {
+  return x * x;
+}
+
+// 클래스
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+// 임의의 함수를 export
+export default y => y * y;
