b2e2acf43d91feaf237daa7f3c850a54afbe4d75
gitul0515 <gitul0515@gmail.com>
2022-01-11 01:11:22 +0900

    코딩 테스트: 섹션 4 뒤집은 소수

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-// console.log(/[a-z]{1}/.test('s'));
-// console.log(/[a-z]{1}/.test(' '));
-// console.log(/[a-z]{1}/.test('?'));
-// console.log(/[a-z]{1}/.test('!'));
-
-const str = 'Hello world';
-const result = str.replace(/[a-z]/g, '');
-console.log(result);
+console.log(Number('00902'));
