952627d8c12d00bf6a551e2a5f1e5211428dcfa6
gitul0515 <gitul0515@gmail.com>
2022-01-31 21:10:41 +0900

    html&css 마크업

--- 드림코딩/리액트/react-basic/habit-tracker/src/app.jsx
+++ 드림코딩/리액트/react-basic/habit-tracker/src/app.jsx
@@ -1,8 +1,33 @@
 import './app.css';
+import React, { Component } from 'react';
 import Habits from './components/habits';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLeaf } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
-  return <Habits />
+class App extends Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <section className='app'>
+        <header className='header'>
+          <FontAwesomeIcon className='header__icon' icon={faLeaf} />
+          <span className='header__title'>Habit Tracker</span>
+          <span className='header__habit-name-count'>0</span>
+        </header>
+
+        <form className='form'>
+          <input className='form__input' type="text" placeholder='Habit'/>
+          <button className='form__btn'>Add</button>
+        </form>
+
+        <Habits />
+
+        <button className='btn-reset'>Reset All</button>
+      </section>
+    )
+  }
 }
 
 export default App;
