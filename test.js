218a9265aa57753e6138a656349bcbdc4c28d181
gitul0515 <gitul0515@gmail.com>
2022-02-12 20:23:34 +0900

    html & css 완성

--- test.js
+++
@@ -1,9 +0,0 @@
-let requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
-
-fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyApe30tsjafvMnnykLW9pjN1YihHEi04PQ", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
