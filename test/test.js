b6560ce5e345c15c0338ae978daa2c7cfc1dd97b
gitul0515 <gitul0515@gmail.com>
2022-02-13 21:06:04 +0900

    무한 슬라이드 구현

--- test/test.js
+++ test/test.js
@@ -1,10 +1,24 @@
-const buttons = [...document.querySelectorAll('input[type=radio]')];
+// 수직형 무한 슬라이드 배너
+const bannerContainer = document.querySelector('.banner-container');
+const offSetY = bannerContainer.getBoundingClientRect().height;
+const bannerInner = document.querySelector('.banner-inner');
 let i = 0;
-let curButton = buttons[i];
+let curBanner;
 
-setInterval(() => {
-  curButton.removeAttribute('checked');
-  curButton = buttons[++i];
-  curButton.setAttribute('checked', '');
-  if (i >= 2) i = -1;
+let timeId = setInterval(() => {
+  curBanner = bannerInner.children[i];
+  bannerInner.appendChild(curBanner.cloneNode(true));
+  i++;
+  bannerInner.style.transform = `translateY(${-offSetY * i}px)`;
 }, 2000);
+
+// const buttons = [...document.querySelectorAll('input[type=radio]')];
+// let i = 0;
+// let curButton = buttons[i];
+
+// setInterval(() => {
+//   curButton.removeAttribute('checked');
+//   curButton = buttons[++i];
+//   curButton.setAttribute('checked', '');
+//   if (i >= 2) i = -1;
+// }, 2000);
