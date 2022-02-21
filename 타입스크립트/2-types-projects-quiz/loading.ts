3eba0a5e1461f195a52ec3679c97ca953bf42f00
gitul0515 <gitul0515@gmail.com>
2022-02-21 23:42:43 +0900

    printLoginState 함수 구현

--- 타입스크립트/2-types-projects-quiz/loading.ts
+++ 타입스크립트/2-types-projects-quiz/loading.ts
@@ -23,4 +23,17 @@
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+
+  function printLoginState(resourceLoadState: ResourceLoadState): void {
+    const { state } = resourceLoadState;
+    if (state === 'loading') {
+      console.log(`👀 ${state}...`);
+    } else if (state === 'success') {
+      console.log(`😃 ${resourceLoadState.response.body}`);
+    } else if (state === 'fail') {
+      console.log(`😱 ${resourceLoadState.reason}`);
+    } else {
+      throw new Error('unexpected error');
+    }
+  }
 }
