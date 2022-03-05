54a62a6ee94db6892f21ef11d97ae2cf7d914fae
gitul0515 <gitul0515@gmail.com>
2022-03-05 16:05:39 +0900

    add abstract class & derived class

--- 타입스크립트/4-oop-project/stack.ts
+++ 타입스크립트/4-oop-project/stack.ts
@@ -1,13 +1,39 @@
 {
-  class Stack {
-    private data = {};
-    private top:number = -1;
+  type StackValue = string | number | boolean;
+  type StackData = {
+    [key: string]: StackValue;
+  }
 
-    public push(str: string) {
-      this.data[++this.top] = str;
+  interface StackMethod {
+    push(value: StackValue): void;
+    pop(): StackValue;
+    peek(): StackValue;
+  }
+
+  abstract class Stack implements StackMethod {
+    protected data: StackData = {};
+    protected top:number = -1;
+
+    public abstract push(value: StackValue): void;
+    public abstract pop(): StackValue;
+    public abstract peek(): StackValue;
+
+    get size(): number {
+      return this.top + 1;
     }
 
-    public pop(): string {
+    get getData(): StackData {
+      return this.data;
+    }
+  }
+
+  // string을 저장하는 스택
+  class StringStack extends Stack {
+    public push(value: string) {
+      this.data[++this.top] = value;
+    }
+
+    public pop(): StackValue {
       if (this.size === 0) {
         return 'stack is empty';
       }
@@ -16,23 +42,95 @@
       return result;
     }
 
-    public peek(): string {
+    public peek(): StackValue {
       if (this.size === 0) {
         return 'stack is empty';
       }
       return this.data[this.top];
     }
+  }
 
-    get size(): number {
-      return this.top + 1;
+  // number을 저장하는 스택
+  class NumberStack extends Stack {
+    public push(value: number) {
+      this.data[++this.top] = value;
     }
 
-    get getData(): object {
-      return this.data;
+    public pop(): StackValue {
+      if (this.size === 0) {
+        return 'stack is empty';
+      }
+      const result = this.data[this.top];
+      delete this.data[this.top--];
+      return result;
+    }
+
+    public peek(): StackValue {
+      if (this.size === 0) {
+        return 'stack is empty';
+      }
+      return this.data[this.top];
     }
   }
 
-  const stack = new Stack();
-  console.log(stack.size);
+  // boolean을 저장하는 스택
+  class BooleanStack extends Stack {
+    public push(value: boolean) {
+      this.data[++this.top] = value;
+    }
+
+    public pop(): StackValue {
+      if (this.size === 0) {
+        return 'stack is empty';
+      }
+      const result = this.data[this.top];
+      delete this.data[this.top--];
+      return result;
+    }
+
+    public peek(): StackValue {
+      if (this.size === 0) {
+        return 'stack is empty';
+      }
+      return this.data[this.top];
+    }
+  }
+
+  const stringStack = new StringStack();
+  stringStack.push('A');
+  stringStack.push('B');
+  stringStack.push('C');
+  console.log(stringStack.pop()); // C
+  console.log(stringStack.peek()); // B
+  console.log(stringStack.pop()); // B
+  console.log(stringStack.pop()); // A
+  console.log(stringStack.pop());
+  console.log(stringStack.size);
+  console.log(stringStack.getData);
+  console.log();
   
+  const numberStack = new NumberStack();
+  numberStack.push(1);
+  numberStack.push(2);
+  numberStack.push(3);
+  console.log(numberStack.pop()); // 3
+  console.log(numberStack.peek()); // 2
+  console.log(numberStack.pop()); // 2
+  console.log(numberStack.pop()); // 1
+  console.log(numberStack.pop());
+  console.log(numberStack.size);
+  console.log(numberStack.getData);
+  console.log();
+
+  const booleanStack = new BooleanStack();
+  booleanStack.push(true);
+  booleanStack.push(false);
+  booleanStack.push(true);
+  console.log(booleanStack.pop()); // true
+  console.log(booleanStack.peek()); // false
+  console.log(booleanStack.pop()); // false
+  console.log(booleanStack.pop()); // true
+  console.log(booleanStack.pop());
+  console.log(booleanStack.size);
+  console.log(booleanStack.getData);
 }
