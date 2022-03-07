48f0ed2e85d1493c22a1e05b25224d3fa8e98e97
gitul0515 <gitul0515@gmail.com>
2022-03-07 15:48:05 +0900

    refator stack class

--- 타입스크립트/4-oop-project/stack-with-ellile.ts
+++ 타입스크립트/4-oop-project/stack-with-ellile.ts
@@ -1,63 +1,68 @@
 {
   // 연결리스트로 구현한 스택
   interface StackInterface {
-    size: number;
+    readonly size: number;
     push(value: string): void;
     pop(): string;
     peek(): string;
   }
 
-  class ListNode {
-    data: string;
-    link: ListNode | null;
-
-    constructor(value: string) {
-      this.data = value; 
-      this.link = null;
+  class StackNode {
+    constructor(readonly data: string, readonly link: StackNode | null) {
     }
   }
 
   class Stack implements StackInterface{
-    size: number = 0;
-    head: ListNode | null = null;
+    private _size: number = 0;
+    get size() {return this._size};
+    head: StackNode | null = null;
 
-    public push(value: string): void {
-      if (this.head === null) {
-        this.head = new ListNode(value);
-      } else {
-        const newNode = new ListNode(value);
-        newNode.link = this.head;
-        this.head = newNode;
-      }
-      this.size++;
+    constructor(private capacity: number) {
     }
 
-    public peek():string {
-      if (this.head === null) {
-        return 'stack is empty';
+    push(value: string): void {
+      if (this._size === this.capacity) {
+        throw new Error('stack is full!');
+      }
+      const newNode = new StackNode(value, this.head);
+      this.head = newNode;
+      this._size++;
+    }
+
+    pop(): string {
+      if (this.head == null) { 
+        throw new Error('stack is empty!');
+      }
+      const result = this.head.data;
+      this.head = this.head.link;
+      this._size--;
+      return result;
+    }
+
+    peek():string {
+      if (this.head == null) {
+        throw new Error('stack is empty!');
       }
       return this.head.data;
     }
 
-    public pop(): string {
-      if (this.head === null) {
-        return 'stack is empty';
-      }
-      const result = this.head.data;
-      this.head = this.head.link;
-      this.size--;
-      return result;
+    addCapacity(capacity: number) {
+      this.capacity += capacity;
     }
   }
 
-  const stack = new Stack();
+  const stack = new Stack(3);
   stack.push('a');
   stack.push('b');
   stack.push('c');
-  console.log(stack.peek()); // c
-  console.log(stack.size); // 3
-  console.log(stack.pop()); // c
-  console.log(stack.pop()); // b
-  console.log(stack.pop()); // a
-  console.log(stack.size); // 0
+  stack.addCapacity(2);
+  stack.push('d');
+  console.log(stack);
+  
+  // console.log(stack.peek()); // c
+  // console.log(stack.size); // 3
+  // console.log(stack.pop()); // c
+  // console.log(stack.pop()); // b
+  // console.log(stack.pop()); // a
+  // console.log(stack.size); // 0
 }
