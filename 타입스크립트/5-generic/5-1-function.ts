5c75d2f734a7bf4bedd981d928ac8f2e62aa4807
gitul0515 <gitul0515@gmail.com>
2022-03-09 18:40:08 +0900

    함수 제네릭 실습

---
+++ 타입스크립트/5-generic/5-1-function.ts
@@ -0,0 +1,27 @@
+{
+  function checkNotNullBad(arg: number | null): number {
+    if (arg == null) {
+      throw new Error('not valid number!');
+    }
+    return arg;
+  }
+
+  function checkNotNullAnyBad(arg: any | null): any {
+    if (arg == null) {
+      throw new Error('not valid number!');
+    }
+    return arg;
+  }
+
+  function checkNotNull<T>(arg: T | null): T {
+    if (arg == null) {
+      throw new Error('not valid number!');
+    }
+    return arg;
+  }
+  const num: number = checkNotNull(123);
+  const bool: boolean = checkNotNull(true);
+
+
+}
+
