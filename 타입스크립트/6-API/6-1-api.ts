6eb343079241b28d08808df755725a450e40c8c6
gitul0515 <gitul0515@gmail.com>
2022-03-12 19:22:00 +0900

    Array 내장 메서드 실습

---
+++ 타입스크립트/6-API/6-1-api.ts
@@ -0,0 +1,23 @@
+type Student = {
+  passed: boolean;
+};
+
+const students: Student[] = [{passed: true}, {passed: true}, {passed: true}];
+const result = students.every(student => {
+  return student.passed;
+});
+console.log(result);
+
+// 타입 확인하기
+class Animal {}
+class Cat extends Animal {
+  isCat: boolean = true;
+}
+class Dog extends Animal {
+  isDog: boolean = false;
+}
+const animals: Animal[] = [new Cat(), new Cat(), new Cat()];
+function isCat(animal: Animal): animal is Cat {
+  return (animal as Cat).isCat !== undefined;
+}
+console.log(animals.every<Cat>(isCat));
