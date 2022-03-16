7556d1659ff22b80d45a9d4715a0553862448cc4
gitul0515 <gitul0515@gmail.com>
2022-03-16 16:50:15 +0900

    인덱스 타입 실습

---
+++ 타입스크립트/8-advanced/8-2-index.ts
@@ -0,0 +1,32 @@
+{
+  const obj = {
+    name: 'gihong',
+  }
+  obj.name;
+  obj['name'];
+
+  type Animal = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+  }
+
+  type Name = Animal['name']; // string
+  const text: Name = 'hello';
+
+  type Gender = Animal['gender']; // 'male' | 'female';
+  const gender:Gender = 'male';
+
+  type Keys = keyof Animal; // 'name' | 'age' | 'gender';
+  const keys:Keys = 'name';
+
+  type Person = {
+    name: string;
+    gender: Animal['gender']; // 'male' | 'female';
+  }
+
+  const person: Person = {
+    name: 'gihong',
+    gender: 'male'
+  }
+}
