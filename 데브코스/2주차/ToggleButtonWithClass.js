8e3ebc9052c0c1147093a9dddef43962e88166b8
gitul0515 <gitul0515@gmail.com>
2022-04-03 19:53:26 +0900

    Feat: 추가 TimerToggleButton & ToggleButtonGroup

--- 데브코스/2주차/ToggleButtonWithClass.js
+++ 데브코스/2주차/ToggleButtonWithClass.js
@@ -5,9 +5,10 @@ class ToggleButton {
     toggled: false
   };
 
-  constructor({target, text}) {
+  constructor({target, text, onClick}) {
     this.target = target;
     this.text = text;
+    this.onClick = onClick;
     this.setup();
   }
 
@@ -27,7 +28,10 @@ class ToggleButton {
   }
 
   bindEvent() {
-    this.button.addEventListener('click', () => this.setState());
+    this.button.addEventListener('click', () => {
+      this.setState();
+      this.onClick && this.onClick(this.state.clickCount);
+    });
   }
 
   setState() {
@@ -39,7 +43,73 @@ class ToggleButton {
   }
 }
 
-const button = new ToggleButton({
+class TimerToggleButton extends ToggleButton {
+  constructor({target, text, onClick, timer}) {
+    super({target, text, onClick});
+    this.timer = timer;
+    this.setInterval();
+  }
+
+  setInterval() {
+    setInterval(() => {
+      this.state = {
+        ...this.state,
+        toggled: !this.state.toggled
+      }
+      this.render();
+    }, this.timer);
+  }
+}
+
+class ToggleButtonGroup {
+  constructor({target, buttons}) {
+    this.target = target;
+    this.buttons = buttons;
+    this.setup();
+  }
+
+  setup() {
+    this.group = document.createElement('div');
+    this.createButtons();
+    this.target.appendChild(this.group);
+  }
+
+  createButtons() {
+    this.buttons.forEach(({ type, ...props }) => {
+      switch (type) {
+        case 'toggle':
+          new ToggleButton({ target: this.group, ...props });
+          break;
+        case 'timerToggle':
+          new TimerToggleButton({ target: this.group, ...props });
+          break;     
+        default:
+          throw new Error('invalid type');
+      }
+    });
+  }
+}
+
+new ToggleButtonGroup({
   target: document.querySelector('body'),
-  text: 'Button1'
+  buttons: [
+    {
+      type: 'toggle',
+      text: 'Button1'
+    },
+    {
+      type: 'toggle',
+      text: 'Button2',
+      onClick: clickCount => {
+        if (clickCount % 3 === 0) {
+          alert('세 번째 클릭입니다.');
+        }
+      }
+    },
+    {
+      type: 'timerToggle',
+      text: 'TimerButton1',
+      timer: 1000
+    }
+  ]
 });
