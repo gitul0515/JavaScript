45490d5469dae14dfec40629a6a907db371555ac
gitul0515 <gitul0515@gmail.com>
2022-04-29 00:56:49 +0900

    feat: App, Header, Keyword 컴포넌트 추가

---
+++ 데브코스/6주차/고양이사진검색기/src/App.js
@@ -0,0 +1,33 @@
+import { request } from './api.js';
+import Header from './Header.js';
+import SuggestKeywords from './SuggestKeywords.js';
+
+export default function App({ $target }) {
+  this.state = {
+    keywords: [],
+  };
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    suggestKeywords.setState(this.state.keywords);
+  };
+
+  const header = new Header({
+    $target,
+    onKeywordInput: async (keyword) => {
+      if (keyword.trim().length > 1) {
+        const keywords = await request(`/keywords?q=${keyword}`);
+
+        this.setState({
+          ...this.state,
+          keywords,
+        });
+      }
+    },
+  });
+
+  const suggestKeywords = new SuggestKeywords({
+    $target,
+    initialState: this.state.keywords,
+  });
+}
---
+++ 데브코스/6주차/고양이사진검색기/src/Header.js
@@ -0,0 +1,14 @@
+import Keyword from './Keyword.js';
+
+export default function Header({ $target, onKeywordInput }) {
+  const $header = document.createElement('header');
+  $header.className = 'Header';
+  $target.appendChild($header);
+
+  const $title = document.createElement('h1');
+  $title.style.textAlign = 'center';
+  $title.innerText = '고양이 사진 검색기';
+  $header.appendChild($title);
+
+  const keyword = new Keyword({ $target: $header, onKeywordInput });
+}
