f9a57a570a36543dc0b19bee1945d179a909302d
gitul0515 <gitul0515@gmail.com>
2022-04-29 17:07:22 +0900

    feat: setState 함수 추가

--- 데브코스/6주차/고양이사진검색기/src/Header.js
+++ 데브코스/6주차/고양이사진검색기/src/Header.js
@@ -1,14 +1,37 @@
 import Keyword from './Keyword.js';
 
-export default function Header({ $target, onKeywordInput }) {
+export default function Header({
+  $target,
+  initialState,
+  onKeywordInput,
+  onEnter,
+}) {
   const $header = document.createElement('header');
   $header.className = 'Header';
   $target.appendChild($header);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    if (this.state.keyword !== nextState.keyword) {
+      this.state = nextState;
+      keyword.setState({
+        value: this.state.keyword,
+      });
+    }
+  };
 
   const $title = document.createElement('h1');
   $title.style.textAlign = 'center';
   $title.innerText = '고양이 사진 검색기';
   $header.appendChild($title);
 
-  const keyword = new Keyword({ $target: $header, onKeywordInput });
+  const keyword = new Keyword({
+    $target: $header,
+    initialState: {
+      keyword: this.state.keyword,
+    },
+    onKeywordInput,
+    onEnter,
+  });
 }
